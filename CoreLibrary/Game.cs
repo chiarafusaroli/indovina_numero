d935358f014621ecd046d43c910988d25272a517
chiara fusaroli <chiara.fusaroli@ispascalcomandini.it>
2025-02-07 16:55:21 +0100

    gestione console

--- CoreLibrary/Game.cs
+++ CoreLibrary/Game.cs
@@ -1,7 +1,48 @@
 ﻿namespace CoreLibrary
 {
-    public class Class1
+    public enum GameStatus
     {
+        LOSE,
+        WIN,
+        IN_PROGRESS
+    }
+    public class Game
+    {
+        private int _maxAttempts;
+        private int _usedAttempts;
+
+        private int _numberToGuess;
+        private GameStatus _status;
+        public GameStatus Status { get { return _status; } }
+
+        public int RemainingAttempts
+        {
+            get { return _maxAttempts - _usedAttempts; }
+        }
+
+        public Game(int maxAttempts, int maxNumeber=1000)
+        {
+            this._maxAttempts = maxAttempts;
+            this._usedAttempts = 0;
+
+            Random random = new Random();
+            this._numberToGuess = random.Next(maxNumeber);
+        }
+
+        public GameStatus Attempts(int attempts)
+        {
+            _usedAttempts += 1;
+            if(attempts == _numberToGuess)
+            {
+                _status = GameStatus.WIN;
+            }
+            if(RemainingAttempts>0)
+                _status = GameStatus.IN_PROGRESS; 
+            else
+                _status = GameStatus.LOSE;
+
+            return Status;
+        }
 
     }
 }
