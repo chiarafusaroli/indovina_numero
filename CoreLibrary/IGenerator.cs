8b795278037ffadb320687a84993566512d2cdb9
chiara fusaroli <chiara.fusaroli@ispascalcomandini.it>
2025-02-07 18:47:32 +0100

    inserimento interfaccia per la gestione del numero da indovinare

---
+++ CoreLibrary/IGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    public interface IGenerator
+    {
+        public int GenerateNumber(int maxNumber);
+    }
+}
---
+++ CoreLibrary/RandomGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    internal class RandomGenerator : IGenerator
+    {
+        int IGenerator.GenerateNumber(int maxNumber)
+        {
+            Random random = new Random();
+            return random.Next(maxNumber);
+        }
+    }
+}
